02dc8d770c532a0ec6a551ea23046d5784ce463b
MTomasCoro <mcoro@frba.utn.edu.ar>
2025-04-05 23:57:25 +0000

    agrego resolucion logger y config

--- client/src/client.c
+++ client/src/client.c
@@ -18,7 +18,7 @@ int main(void)
 
 	// Usando el logger creado previamente
 	// Escribi: "Hola! Soy un log"
-
+	log_info(logger, "Hola! Soy un log\n");
 
 	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
 
@@ -27,8 +27,13 @@ int main(void)
 	// Usando el config creado previamente, leemos los valores del config y los 
 	// dejamos en las variables 'ip', 'puerto' y 'valor'
 
+	ip = config_get_string_value(config, "IP");
+	puerto = config_get_string_value(config, "PUERTO");
+	valor = config_get_string_value(config, "CLAVE");
+
 	// Loggeamos el valor de config
 
+	log_info(logger, "Se leyeron los siguiente valores:\n ip:%s\n puerto:%s\n clave:%s\n", ip, puerto, valor);
 
 	/* ---------------- LEER DE CONSOLA ---------------- */
 
@@ -56,12 +61,27 @@ t_log* iniciar_logger(void)
 {
 	t_log* nuevo_logger;
 
+	nuevo_logger = log_create("tp0.log", "TP0", 1, LOG_LEVEL_INFO);
+
+	if (nuevo_logger == NULL)
+	{
+		printf("No se pudo crear el log\n");
+		exit(1);
+	}
+	
 	return nuevo_logger;
 }
 
 t_config* iniciar_config(void)
 {
 	t_config* nuevo_config;
+	nuevo_config = config_create("./cliente.config");
+
+	if (nuevo_config == NULL)
+	{
+		printf("No se pudo crear el config\n");
+		exit(2);
+	}
 
 	return nuevo_config;
 }
@@ -71,13 +91,16 @@ void leer_consola(t_log* logger)
 	char* leido;
 
 	// La primera te la dejo de yapa
-	leido = readline("> ");
+	//leido = readline("> ");
 
 	// El resto, las vamos leyendo y logueando hasta recibir un string vacío
-
-
+	while (strcmp(leido = readline("> "), ""))
+	{
+		log_info(logger, leido);
+	}
 	// ¡No te olvides de liberar las lineas antes de regresar!
 
+	free(leido);
 }
 
 void paquete(int conexion)
@@ -97,4 +120,12 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
+
+	if(logger != NULL){
+		log_destroy(logger);
+	}
+
+	if(config != NULL){
+		config_destroy(config);
+	}
 }
